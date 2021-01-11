a3480d90407c0cc1f939a86dd4953449f2179914
hermankongen <37117219+hermankongen@users.noreply.github.com>
2021-01-11 10:37:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function () { //når knap a blir trykket på så printer den herman
     basic.showString("Herman")
 })
 basic.forever(function () {
